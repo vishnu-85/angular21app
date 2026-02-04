172fb65a29b022b9c0150c86905cf7110ff28fbe
vishnu-85 <vishnukantlav291@gmail.com>
2026-02-04 21:51:49 +0530

    init ang 21 app code

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  protected readonly title = signal('angular21');
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
+};
